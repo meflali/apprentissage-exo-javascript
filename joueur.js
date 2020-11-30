29afbde179c85bc5eaef044aa9bb811387c688a9
mef-lang <66832069+mef-lang@users.noreply.github.com>
2020-11-30 20:55:33 +0100

    Ajout objet carac

--- joueur.js
+++ joueur.js
@@ -1,20 +1,24 @@
 var joueur = {
     nom : "Hulk",
     description : "Le meilleur du joueur",
-    force : 8,
-    agilite : 7,
-    intel : 5,
+    carac : {
+        force : 8,
+        agilite : 7,
+        intel : 5,
+    }
 }
 
 
+
 afficherInfoJoueur(joueur);
-
-
-
-
-
 function afficherInfoJoueur(player){
     for( var proportie in player){
-        console.log(proportie + " : " + player[proportie]);
+        if(typeof(player[proportie]) !== "object"){
+            console.log(proportie + " : " + player[proportie]);
+        } else {
+            for( var subproportie in player[proportie]){
+                console.log(subproportie + " : " + player[proportie][subproportie]);
+            }
+        }
     }
 }
