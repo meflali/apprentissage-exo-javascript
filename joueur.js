2585d387eda64f2ab377187f22d83252a4922d94
mef-lang <66832069+mef-lang@users.noreply.github.com>
2020-12-01 22:35:17 +0100

    Update joueur.js
    creation affichage des joueurs

--- joueur.js
+++ joueur.js
@@ -1,24 +1,43 @@
 var joueur = {
     nom : "Hulk",
     description : "Le meilleur du joueur",
+    level : 1,
+    pv : 100,
     carac : {
         force : 8,
         agilite : 7,
         intel : 5,
+    },
+    afficherMonJoueur: function (){
+    console.log(" Nom : " + this.nom);
+    console.log(" Description : " + this.description);
+    console.log(" Points de vie : " + this.pv);
+    console.log(" Level : " + this.level);
+    console.log(" Force : " + this.carac.force);
+    console.log(" Agility : " + this.carac.agilite);
+    console.log(" Intelligence : " + this.carac.intel);
+    },
+
+    levelUp : function(){
+        this.level++;
+        this.pv += 10;
+        this.carac.force += 2;
+        this.carac.agilite ++;
     }
 }
+module.exports = joueur;
 
 
 
-afficherInfoJoueur(joueur);
-function afficherInfoJoueur(player){
-    for( var proportie in player){
-        if(typeof(player[proportie]) !== "object"){
-            console.log(proportie + " : " + player[proportie]);
-        } else {
-            for( var subproportie in player[proportie]){
-                console.log(subproportie + " : " + player[proportie][subproportie]);
-            }
-        }
-    }
-}
+//afficherInfoJoueur(joueur);
+//function afficherInfoJoueur(player){
+    //for( var proportie in player){
+       // if(typeof(player[proportie]) !== "object"){
+           // console.log(proportie + " : " + player[proportie]);
+       // } else {
+            //function afficherInfoJoueur(player[proportie]){
+
+           // }
+        //}
+    //}
+//}
