26fd7db06477ed0c363028eb12b84dd27ce5ece4
mef-lang <66832069+mef-lang@users.noreply.github.com>
2020-12-01 22:34:58 +0100

    Create ennemi.js
    creatinodes ennemi

---
+++ ennemi.js
@@ -0,0 +1,50 @@
+var templateEnnemi = {
+    creerThanos: function () {
+        var ennemi1 = {
+            nom: "Thanos1",
+            description: "Maitre du mal",
+            level: 1,
+            force: 2,
+            pv : 4,
+
+            afficherEnnemi: function () {
+                var txt = "Nom : " + this.nom + "\n";
+                txt += "Description : " + this.description + "\n";
+                txt += "Level : " + this.level + "\n";
+                txt += "Points de vie : " + this.pv + "\n";
+                txt += "Force : " + this.force;
+                console.log(txt);
+            }
+        }
+        return ennemi1;
+    },
+
+    creerVenom : function (){
+        var ennemi2 = {
+            nom : "Venom",
+            description : "Monstre",
+            level : 2,
+            force : 4,
+            pv : 7,
+
+            afficherEnnemi : function() {
+                var txt = "Nom : " + this.nom + "\n";
+                txt += "Description : " + this.description + "\n";
+                txt += "Level : " + this.level + "\n";
+                txt += "Points de vie : " + this.pv + "\n";
+                txt += "Force : " + this.force ;
+                console.log(txt);
+            }
+        }
+        return ennemi2;
+    }
+}
+module.exports = templateEnnemi;
+
+
+
+
+
+/* MAIN   */
+
+
